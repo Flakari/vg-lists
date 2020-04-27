eddae7684a38c727f7d1872cbcf56ccd3994e33c
Flakari <natedimock@yahoo.com>
2020-04-27 16:20:31 -0500

    Added SASS to project to reorganize CSS and began work on making header responsive. Header buttons currently have no functionality.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './Header.scss';
 import Searchbar from '../Searchbar/Searchbar';
 
 const Header = (props) => {
     return (
         <header>
+            <button><img src={require("../../images/Hamburger_icon.svg")} alt=""></img></button>
             <p>VG Lists</p>
-            <Searchbar setData={props.setData}/>
-            <img src={require("../../images/magnifying-glass.svg")} alt="Search"></img>
+            <div id="search-container">
+                <Searchbar setData={props.setData}/>
+            </div>
+            <button><img src={require("../../images/magnifying-glass.svg")} alt="Search"></img></button>
+            <div id="mobile-search-container">
+                <Searchbar setData={props.setData}/>
+            </div>
         </header>
     );
 };
