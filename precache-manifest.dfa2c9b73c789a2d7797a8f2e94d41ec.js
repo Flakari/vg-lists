423009e1c3bdf05a1d6fc042e99b62c2aeca45d2
Flakari <natedimock@yahoo.com>
2020-05-11 13:20:11 -0500

    Updates

---
+++ precache-manifest.dfa2c9b73c789a2d7797a8f2e94d41ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01af2e383ed8afdd74cb28b40742aa4d",
+    "url": "/vg-lists/index.html"
+  },
+  {
+    "revision": "582c6ed6a1cc59258ebd",
+    "url": "/vg-lists/static/css/main.3a066594.chunk.css"
+  },
+  {
+    "revision": "04dfbb77e68860f63ba7",
+    "url": "/vg-lists/static/js/2.f246c082.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/vg-lists/static/js/2.f246c082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582c6ed6a1cc59258ebd",
+    "url": "/vg-lists/static/js/main.3ef7d3bb.chunk.js"
+  },
+  {
+    "revision": "74ddd1073a0952b4b209",
+    "url": "/vg-lists/static/js/runtime-main.7477fb1d.js"
+  },
+  {
+    "revision": "6a12383b6d42c021856a4f35e347bb3b",
+    "url": "/vg-lists/static/media/Delete_icon.6a12383b.svg"
+  },
+  {
+    "revision": "54ebc46aeaef815529e319569026e361",
+    "url": "/vg-lists/static/media/Hamburger_icon.54ebc46a.svg"
+  },
+  {
+    "revision": "f8366a62048bf839038c53c14a149298",
+    "url": "/vg-lists/static/media/magnifying-glass.f8366a62.svg"
+  }
+]);
